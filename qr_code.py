e7bb37fa677d382dff3c2144f872d2e5c6a099ab
Anshu-Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2024-06-05 19:06:41 +0530

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,3 +1,15 @@
-import qrcode as qr
-img=qr.make("https://www.youtube.com/")
-img.save("yt.png")
+# #Basic qr code to genereate output
+# import qrcode as qr
+# img=qr.make("https://www.youtube.com/")
+# img.save("yt.png")
+
+#Advanced 
+import qrcode
+from PIL import Image
+qr=qrcode.QRCode(#version=1,
+          error_correction=qrcode.constants.ERROR_CORRECT_H,
+          box_size=12,border=5)
+qr.add_data("https://www.youtube.com/")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="black")
+img.save("ytlink.png")
